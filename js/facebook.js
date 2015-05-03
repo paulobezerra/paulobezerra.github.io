881c97a9f8a074765828b7ac217bd86c2f51c758
Paulo Bezerra <bezerra.paulo@gmail.com>
2015-05-03 10:29:27 -0400

    Voltando.

--- js/facebook.js
+++ js/facebook.js
@@ -14,6 +14,6 @@ window.fbAsyncInit = function () {
     }
     js = d.createElement(s);
     js.id = id;
-    //js.src = "//connect.facebook.net/pt_BR/sdk.js";
+    js.src = "//connect.facebook.net/pt_BR/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
