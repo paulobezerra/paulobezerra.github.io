4c576b54f8d5e8d2f1598fe47a4b21367250f1f8
Paulo Bezerra <bezerra.paulo@gmail.com>
2015-05-10 10:45:47 -0400

    Alterando url comentários do face

--- app/controllers/PostController.js
+++ app/controllers/PostController.js
@@ -1,5 +1,6 @@
 angular.module('meuSite')
-        .controller('PostController', ['$scope', function ($scope) {
+        .controller('PostController', ['$scope', '$location', function ($scope, $location) {
                 var postJSON = window.sessionStorage.getItem("post");
                 $scope.post = JSON.parse(postJSON);
+                $scope.urlFacebook = window.location.href;
             }]);
