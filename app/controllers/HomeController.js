681a6bb8993e4b73791a8d0d76090afa4bf3753a
Paulo Bezerra <bezerra.paulo@gmail.com>
2015-05-14 13:14:50 -0400

    acrescentando curtir e compartilhar

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -6,7 +6,7 @@ angular.module('meuSite')
                         });
                         
                         $scope.irParaPost = function(post){
-                            window.sessionStorage.setItem('post', JSON.stringify(post));
+                            window.localStorage.setItem('post', JSON.stringify(post));
                             $location.path('post/'+post.url);
                             window.fbAsyncInit();
                         };
--- app/controllers/PostController.js
+++ app/controllers/PostController.js
@@ -1,6 +1,6 @@
 angular.module('meuSite')
         .controller('PostController', ['$scope', '$location', function ($scope, $location) {
-                var postJSON = window.sessionStorage.getItem("post");
+                var postJSON = window.localStorage.getItem("post");
                 $scope.post = JSON.parse(postJSON);
                 $scope.urlFacebook = window.location.href;
             }]);
